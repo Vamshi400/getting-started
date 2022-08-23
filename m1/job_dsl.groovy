a9b2a350e8c3e0d3fba2881d7596c3c01b73868f
Vamshi <96587913+Vamshi400@users.noreply.github.com>
2022-08-23 20:16:31 +0530

    Create job_dsl.groovy

---
+++ m1/job_dsl.groovy
@@ -0,0 +1,32 @@
+
+def pipelines = [
+    [name: 'm1/job1', scriptPath: 'm1/Jenkinsfile1'],
+    [name: 'm1/job2', scriptPath: 'm1/Jenkinsfile2']
+]
+
+for(p in pipelines) {
+    pipelineJob("${p.name}") {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            name('github')
+                            url('https://github.com/Vamshi400/getting-started.git')
+                        }
+                        branch('master')
+                        extensions {
+                            cloneOptions {
+                                shallow(true)
+                                depth(1)
+                                noTags(true)
+                            }
+                        }
+                    }
+                }
+                scriptPath("${p.scriptPath}")
+            }
+        }
+        }
+    }
+}
